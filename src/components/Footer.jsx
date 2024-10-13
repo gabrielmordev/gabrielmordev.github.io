7d5ffbf649bd57b620e48ce04f6e90d79de887e3
a23momuga <a23momuga@iesgrancapitan.org>
2024-10-14 00:11:03 +0200

    intento arreglo

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,26 @@
-import Redes from './RedesSociales';
-
+import logoGitHub from "../assets/images/github-mark-white.svg";
+import logoLinkedin from "../assets//images/linkedin.svg";
 
 const Footer = () => {
   return (
     <footer>
-      <p>© 2024 Gabriel Mordev. Todos los derechos reservados.</p>
-        <Redes />
+      <div className="footer-container">
+        <div className="copyright">
+          <p>© 2024 Gabriel Mordev. Todos los derechos reservados.</p>
+        </div>
+        <div className="socials">
+          <a href="https://github.com/gabrielmordev">
+            <img src={logoGitHub} alt="GitHub" />
+          </a>
+          <a href="https://www.linkedin.com/in/gabriel-moreno-mu%C3%B1oz/">
+            <img src={logoLinkedin} alt="LinkedIn" />
+          </a>
+        </div>
+      </div>
     </footer>
   );
 };
 
 export default Footer;
+
+
