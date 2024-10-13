3a188a0e21a22a257ed147f892b585ab8113d390
a23momuga <a23momuga@iesgrancapitan.org>
2024-10-13 14:51:35 +0200

    añadido build (intento arreglo 2)

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ import About from '../pages/About';
 const Header = () => {
   return (
 
-    <Router>
+    <Router basename="/gabrielmordevportfolio">
     <header>
       <h1>GabrielMorDev</h1>
       <nav>
