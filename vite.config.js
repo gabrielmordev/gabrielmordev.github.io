f8fd34af4bd8dafaa9830832012e7372ce1b2dfc
a23momuga <a23momuga@iesgrancapitan.org>
2024-10-13 23:59:55 +0200

    Updates

--- vite.config.js
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  build: {
-    outDir: 'build', // Esto especifica que la carpeta de salida se llamará "build"
-  },
-});
